572f2e973d5d73413ad5e617764a1da235dbed08
Liz Love <love.elizabeth1989@gmail.com>
2020-05-13 03:40:14 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,24 @@
+def using_concat(array, array2)
+  array.concat(array2)
+end
+
+def using_insert(array, element)
+  array.insert(4, element)
+  array
+end
+
+def using_uniq(array)
+  array.uniq
+end
+
+def using_flatten(array)
+  array.flatten
+end
+
+def using_delete(array, string)
+  array.delete(string)
+end
+
+def using_delete_at(array, number)
+  array.delete_at(number)
+end
